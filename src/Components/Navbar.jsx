5477da3a92d39ac9f1c27ae00332911b714b6981
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-03-08 19:05:19 +0500

    home link added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,11 +7,13 @@ const Navbar = ({ isDarkTheme, setIsDarkTheme }) => {
   return (
     <div className="bg-[#b3b3b32c] backdrop-blur-md shadow-custom fixed z-50 top-0 left-0 w-full flex justify-between items-center gap-5 px-5 py-2">
       <div>
-        <img
-          src="/images/logo-removebg.webp"
-          alt="logo"
-          className="min-w-[130px] w-[130px]"
-        />
+        <Link to={"/"}>
+          <img
+            src="/images/logo-removebg.webp"
+            alt="logo"
+            className="min-w-[130px] w-[130px]"
+          />
+        </Link>
       </div>
       <div className="flex items-center justify-center gap-3">
         <Link to={"/create"}>
